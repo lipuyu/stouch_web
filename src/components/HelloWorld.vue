86c35fd64371098259d65864f7dba61a2e2ca7f8
lipuyu <827893047@qq.com>
2023-03-03 10:52:01 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+import { storeToRefs } from "pinia";
+
+const store = useCounterStore();
+const { count, doubleCount } = storeToRefs(store);
+const { increment } = store;
+increment();
+
+class Person {
+  age: number;
+  name: string;
+
+  constructor(age: number, name: string) {
+    this.age = age;
+    this.name = name;
+  }
+}
+
+defineProps<{
+  msg: string
+}>();
+const person = new Person(12, "佩奇");
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }} {{ person.age }} - {{ person.name }} - {{ count + doubleCount }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <span>用户名:</span><input type="text"/>
+    </div>
+    <div>
+      <span>密码:</span><input type="password"/>
+    </div>
+    <div>
+      <button>登录</button>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
