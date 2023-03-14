d06b30414fcd9e6769679a1473687844519ba2e8
lipuyu <827893047@qq.com>
2023-03-14 10:26:58 +0800

    add code

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,12 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     proxy: {
-      "/api/": "http://localhost:8000"
-    }
-  }
+      "/api/": "http://localhost:8000",
+    },
+  },
 });
