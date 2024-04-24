319f4aa646ea54aaec228d2a169c35b73cfffd97
lipuyu <827893047@qq.com>
2024-04-24 14:21:33 +0800

    话茬开发

--- src/components/topic/TopicComponent.vue
+++ src/components/topic/TopicComponent.vue
@@ -3,15 +3,30 @@ import UserAvatar from "@/components/user/UserAvatarComponent.vue";
 import { ref } from "vue";
 
 const text = ref("吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？" +
+  "吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？" +
+  "吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？" +
   "吃面不加蛋是否违规？吃面不加蛋是否违规？吃面不加蛋是否违规？");
 </script>
 
 <template>
   <div class="topic_com">
-    <user-avatar :avatar="''" :userId="2"></user-avatar>
-    <p>{{ text }}</p>
+    <div>
+      <user-avatar :avatar="''" :userId="2"></user-avatar>
+    </div>
+    <div>
+      <div>
+        话茬用户
+      </div>
+      <div>
+        <p>{{ text }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.topic_com {
+  display: grid;
+  grid-template-columns: 54px 1fr;
+}
 </style>
